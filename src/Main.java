c81343e6b20b4b09694397b47a39b1c2c5d25bdb
Michelle <elvirasnchezmichellle@gmail.com>
2024-09-10 15:24:20 -0600

    "Agregue mi clase grupo"

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("---------Menu institucional--------¨)
+                switch (
+
+        )
+    }
 
 }
