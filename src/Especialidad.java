d63797df1fe5241cd80e45f52c4b73e00397708a
iamcris288 <goldmaster288pro@gmail.com>
2024-09-11 15:22:55 -0600

    "modifique la clase Especialidad y añadi diferentes variables"

---
+++ src/Especialidad.java
@@ -0,0 +1,66 @@
+import java.util.List;
+
+public class Especialidad {
+    private String nombre;
+    private String descripcion;
+    private List<String> asignaturas;
+    private int duracionSemestres;
+
+    // Constructor
+    public Especialidad(String nombre, String descripcion, List<String> asignaturas, int duracionSemestres) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.asignaturas = asignaturas;
+        this.duracionSemestres = duracionSemestres;
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<String> getAsignaturas() {
+        return asignaturas;
+    }
+
+    public void setAsignaturas(List<String> asignaturas) {
+        this.asignaturas = asignaturas;
+    }
+
+    public int getDuracionSemestres() {
+        return duracionSemestres;
+    }
+
+    public void setDuracionSemestres(int duracionSemestres) {
+        this.duracionSemestres = duracionSemestres;
+    }
+
+    // Método para agregar una asignatura a la especialidad
+    public void agregarAsignatura(String asignatura) {
+        this.asignaturas.add(asignatura);
+    }
+
+    // Método para mostrar información de la especialidad
+    public void mostrarInformacion() {
+        System.out.println("Especialidad: " + nombre);
+        System.out.println("Descripción: " + descripcion);
+        System.out.println("Duración en semestres: " + duracionSemestres);
+        System.out.println("Asignaturas:");
+        for (String asignatura : asignaturas) {
+            System.out.println("- " + asignatura);
+        }
+    }
+}
+
